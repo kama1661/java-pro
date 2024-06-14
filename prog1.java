66d767ec8ef0c6fd2443596ec8dcc26ba515f947
kama1661 <141501835+kama1661@users.noreply.github.com>
2024-06-14 18:44:51 +0530

    Update prog1.java

--- prog1.java
+++ prog1.java
@@ -1,5 +1,12 @@
 public class prog1 {
+
   public static void main(String[] args) {
-    System.out.println("Hello World");
+
+
+     for(int i = 0; i<=10; i++) {
+
+        System.out.println("Hello Students");
+      }
+
   }
 }
